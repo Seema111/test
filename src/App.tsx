ad648577a3998856904dfd3f890e4399fec9e67b
seema.thapa <seema.thapa@innovatetech.co>
2022-09-09 17:34:28 +0545

    update new changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App" style={{background: 'red'}}>
-      Seema Thapa 
-      Deployment Demo
+    <div className="App" style={{ background: "red" }}>
+      Seema Thapa Deployment Demo
+      New branch Test
+      
     </div>
   );
 }
